c8895a367bf65922bade6696162f7056eec034a9
Justin Spegele <justin.spegele@gmail.com>
2020-05-20 17:46:33 -0400

    added login/register styles

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -26,18 +26,24 @@ export default class Login extends React.Component {
   }
   render() {
     return (
-      <div>
-        <h1>Short Lnk</h1>
+      <div className="boxed-view">
+        <div className="boxed-view__box">
+          <h1>Short Lnk</h1>
 
-        {this.state.error && <p>{this.state.error}</p>}
+          {this.state.error && <p>{this.state.error}</p>}
 
-        <form onSubmit={this.onSubmit} noValidate>
-          <input type="email" ref="email" name="email" placeholder="Email" />
-          <input type="password" ref="password" name="password" placeholder="Password" />
-          <button>Login</button>
-        </form>
+          <form
+            className="boxed-view__form"
+            onSubmit={this.onSubmit}
+            noValidate
+          >
+            <input type="email" ref="email" name="email" placeholder="Email" />
+            <input type="password" ref="password" name="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
 
-        <Link to='/signup'>Need an account?</Link>
+          <Link to='/signup'>Need an account?</Link>
+        </div>
       </div>
     )
   }
--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default () => (
-  <div>NotFound component</div>
+  <div className="boxed-view">
+    <div className="boxed-view__box">
+      <h1>Page Not Found</h1>
+      <p>We were unable to find that page :-(</p>
+      <Link to="/">Head Home</Link>
+    </div>
+  </div>
 )
